943895afd51a0da109e76f077fae8f4e3ef5306d
Dan Morse <djmthedev@gmail.com>
2020-06-16 16:20:08 -0700

    initial

---
+++ GetHeartRates/GetHeartRates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetHeartRates
+//
+//  Created by morse on 6/16/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
